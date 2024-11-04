b851f677cb1254684bab0463d59c93c9bcd2311b
RickyMandich <ricky.mandich@gmail.com>
2024-11-04 11:51:38 +0100

    aggiornamento 2024 11 04 11:51

---
+++ mihai.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>array associativi</title>
+    </head>
+    <body>
+        <!--Scrivere un programma PHP che dichiara un array associativo di dati personali (nome, cognome, età) e lo stampa-->
+        <h1>dati personali</h1>
+        <hr>
+        <!--Scrivere un programma PHP che dichiara un array associativo di voti degli studenti (nome => voto) e calcola la media dei voti.-->
+        <h1>voti degli studenti</h1>
+        <hr>
+        <!--Scrivere un programma PHP che dichiara un array associativo di prodotti (nome => prezzo) e stampa i prodotti con un prezzo superiore a 50 euro.-->
+        <h1>prodotti superiori a 50€</h1>
+        <hr>
+        <!--Scrivere un programma PHP che dichiara un array associativo di parole (parola => lunghezza) e stampa le parole più lunghe-->
+        <h1>parole lunghe</h1>
+        <hr>
+        <!--Scrivere un programma PHP che dichiara un array associativo di mesi dell'anno (numero => nome) e stampa i mesi in ordine crescente utilizzando la funzione "asort".-->
+        <h1>mesi in ordine</h1>
+        <hr>
+    </body>
+    <style>
+        h1{
+            text-transform: uppercase;
+        }
+    </style>
+</html>
