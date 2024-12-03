3a42dff11b18982b62dbf89b80290818c60f0067
RickyMandich <ricky.mandich@gmail.com>
2024-12-03 11:58:47 +0100

    aggiornamento 2024 12 03 11:58

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<?php
+    function scanDirectory($directory){
+        $dir = @opendir($directory);
+        try{
+            while (($file = readdir($dir)) !== false) {
+                if ($file != '.' && $file != '..') {
+                    if(is_dir("$directory/$file")){
+                        scanDirectory("$directory/$file");
+                    }else{
+                        ?>
+                        <li>
+                            <a href="/<?php echo $directory/$file?>"><?php echo $directory/$file?></a>
+                        </li>
+                        <?php
+                    }
+                }
+            }
+            closedir($dir);
+        }catch(Error $e){}
+    }
+?>
+<body>
+    <ul>
+        <?php scanDirectory(".")?>
+    </ul>
+</body>
+</html>
