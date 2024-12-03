30036a34f5e5b03acb6f89b7bf82ff565625cde5
RickyMandich <ricky.mandich@gmail.com>
2024-12-03 12:15:14 +0100

    aggiornamento 2024 12 03 12:15

--- index.php
+++ index.php
@@ -7,30 +7,30 @@
 </head>
 <?php
     function scanDirectory($directory){
+        $ret = [];
         $dir = @opendir($directory);
         try{
             while (($file = readdir($dir)) !== false) {
                 echo "$directory/$file<br>";
                 if ($file != '.' && $file != '..') {
                     if(is_dir("$directory/$file")){
-                        scanDirectory("$directory/$file");
+                        array_push($ret, scanDirectory("$directory/$file"));
                     }else{
                         echo "$directory/$file<br>";
-                        ?>
-                        <li>
-                            <a href="/<?php echo $directory/$file?>"><?php echo $directory/$file?></a>
-                        </li>
-                        <?php
+                        array_push($ret, "<a href=\"/<?php echo $directory/$file?>\"><?php echo $directory/$file?></a>");
                     }
                 }/**/
             }
             closedir($dir);
         }catch(Error $e){}
+        return $ret;
     }
+
+    $file = scanDirectory(".");
 ?>
 <body>
     <ul>
-        <?php scanDirectory(".")?>
+        <?php ?>
     </ul>
 </body>
 </html>
