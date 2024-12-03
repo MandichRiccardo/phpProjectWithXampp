0a8562885b252ed293ad633f5c678d7648401d13
RickyMandich <ricky.mandich@gmail.com>
2024-12-03 16:07:38 +0100

    aggiornamento 2024 12 03 16:07

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
                     echo "&nbsp;&nbsp;";
                 }
                 //assegno a dir la cartella di appartenenza del file in questione
-                echo "$i-->";
+                echo "$value-->";
                 printlnd($value, $deep+1);
             }
             unset($j);
